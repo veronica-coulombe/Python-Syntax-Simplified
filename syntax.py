9a98c43372e6e5c1678fd4318ca693b7326201bf
Veronica Coulombe <156690411+veronica-coulombe@users.noreply.github.com>
2025-08-13 16:41:52 -0400

    Add files via upload

---
+++ syntax.py
@@ -0,0 +1,30 @@
+#variables
+x = 1
+x = x + 2
+print(x)
+
+#strings
+x = "abc"
+y = "de"
+z = x + y
+z
+
+z = z[1:3]
+z
+
+#functions
+def f(x):
+    y = x + 2
+    return y
+f(5)
+
+#if statements
+x = 2
+if x + 1 == 3:
+    print(x)
+
+#loops
+for i in range(1,000):
+    if i % 2 == 0:
+        print(i)
+
